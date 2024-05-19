50a2b00dcd049827c49568962506ccad8258d11f
dferreira-ds <66534246+dferreira-ds@users.noreply.github.com>
2024-05-19 16:11:42 -0500

    Add files via upload

--- client/src/components/ChatWidget/ChatWidget.jsx
+++ client/src/components/ChatWidget/ChatWidget.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { Widget } from "@ryaneewx/react-chat-widget"
+import React, { useEffect } from "react";
+import { Widget, addResponseMessage } from "@ryaneewx/react-chat-widget"
 import { useConvesationStore } from "../../store/useConversationStore";
 import { useMessageStore } from "../../store/useMessageStore";
 import '@ryaneewx/react-chat-widget/lib/styles.css';
 
 import io from "socket.io-client";
-
-const socket = io("http://localhost:5000");
+const socket = io(process.env.REACT_APP_NGROK_URL);
 
 const ChatWidget = () => {
     const createConversation = useConvesationStore(state => state.createConversation);
@@ -14,14 +13,24 @@ const ChatWidget = () => {
     const createMessage = useMessageStore(state => state.createMessage);
 
     const handleNewMessage = async (message) => {
-        e
-        //await createMessage(conversationSid, inboundMessage);
+        await createMessage(conversationSid, message);
         socket.emit("message", message);
     };
 
     const handleToggle = async () => {
         await createConversation();
     };
+
+    useEffect(() => {
+        const handleFlexMessage = (data) => {
+            if (data.Source === "SDK") {
+                addResponseMessage(data.Body);
+            };
+        };
+
+        socket.on("message", handleFlexMessage)
+        return () => socket.off("message", handleFlexMessage);
+    }, []);
 
     return (
         <Widget
